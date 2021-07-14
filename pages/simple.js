f32e6ca57f7198155ca492e6759e91f3bf08d0fc
Alam, Ashraf <joy_csharp@yahoo.com>
2021-07-14 14:14:19 +0100

    update

--- pages/simple.js
+++ pages/simple.js
@@ -1,6 +1,6 @@
 import Link from '@/components/Link'
 
-export default function SimpleTextPage() {
+export default function Simple() {
     return (
         <div className="flex flex-col items-start justify-start md:justify-center md:items-center md:flex-row md:space-x-6 md:mt-24">
             <div className="pt-6 pb-8 space-x-2 md:space-y-5">
