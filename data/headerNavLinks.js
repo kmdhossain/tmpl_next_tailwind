9af549cb25ba1d1bccac03295342df09fb128ebd
K Hossain <tahs.sumon@gmail.com>
2021-11-04 17:49:56 +0000

    update

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -2,7 +2,8 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'My Projects' },
-  { href: '/about', title: 'About' },
+    { href: '/about', title: 'About' },
+    { href: '/team', title: 'Team' },
 ]
 
 export default headerNavLinks
